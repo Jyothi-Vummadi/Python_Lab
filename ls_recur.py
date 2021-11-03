ec76bc3cdfa5f36ed61437ddfb4a5627ec9e64a2
Jyothi-Vummadi <90172590+Jyothi-Vummadi@users.noreply.github.com>
2021-11-03 12:40:48 +0530

    Delete ls_recur.py

--- ls_recur.py
+++
@@ -1,8 +0,0 @@
-def ls_recur(lst,key):
-    for i in range(len(lst)):
-        if lst[i] == key:
-            return i+1
-        else:
-            return(ls_recur(lst[i+1: ],key))
-    return False
-print(ls_recur([34,57,18,59,26,89,30,10],90))
